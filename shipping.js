568bd086fd4b1545ccb996f6ec6c2316cbc1048c
jacob <jacobrchrist186@gmail.com>
2026-04-05 14:52:42 -0400

    Added AJAX

--- shipping.js
+++ shipping.js
@@ -40,7 +40,20 @@ document.getElementById("shippingForm").addEventListener("submit", function(e) {
       carrier,
       method
     };
-
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(shippingData)
+})
+.then(response => response.json())
+.then(data => {
+  console.log("Success:", data);
+})
+.catch(error => {
+  console.error("Error:", error);
+});
     alert("Shipping info saved!");
   }
 });
